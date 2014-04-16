6b4d6f0ad02fb78ab3201010cb160707572e993b
Aaron Bieber <deftly@gmail.com>
2014-04-16 08:29:50 -0600

    iso location

--- first_boot.sh
+++ first_boot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vmadm list | grep $1 | awk '{print "cp /zones/isos/install54_i386.iso /zones/"$1"; vmadm boot " $1 "order=cd,once=d cdrom=/zones/"$1"/install54_i386.iso,ide"}'
+vmadm list | grep $1 | awk '{print "cp /zones/isos/install54_i386.iso /zones/"$1"/root; vmadm boot " $1 " order=cd,once=d cdrom=/install54_i386.iso,ide"}'
